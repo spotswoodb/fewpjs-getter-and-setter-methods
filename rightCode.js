3c41954dcdd2a2bf98d10b1650ad64c8af602d5d
Ben Spotswood <spotswoodb@gmail.com>
2021-09-24 13:02:53 -0500

    Done.

---
+++ rightCode.js
@@ -0,0 +1,33 @@
+// const pi = Math.PI
+
+// class Circle {
+//     // accept 1 parameter called radius and us this.radius to store the value
+//     constructor(radius){
+//         this.radius = radius;
+//     }
+
+//     // define getter methods which will calculate each value using this.raidus and pi
+//     get diameter() {
+//         return 2 * this.radius; 
+//     }
+
+//     set diameter( diameter ) {
+//         this.radius = diameter / 2
+//     } 
+
+//     get circumference() {
+//         return ( this.radius * 2 ) * pi
+//     }
+
+//     set circumference( circumference ) {
+//         this.radius = circumference / ( pi * 2 )
+//     }
+
+//     get area() {
+//         return pi * ( this.radius * this.radius )
+//     }
+
+//     set area( area ) {
+//         this.radius = Math.sqrt( area / pi )
+//     }
+// }
