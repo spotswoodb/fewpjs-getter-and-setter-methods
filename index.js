8c858392f749c2b07da3a0c261b193e23c471e8e
Ben Spotswood <spotswoodb@gmail.com>
2021-09-24 13:02:38 -0500

    commit tree 82a1175acfcb368c8c605b69feeedee8f64c8909

--- index.js
+++ index.js
@@ -2,36 +2,32 @@
 
 // create a class of Circle
 
-const pi = Math.PI
+const pi = Math.PI;
 
 class Circle {
-    // accept 1 parameter called radius and us this.radius to store the value
+    
     constructor(radius){
-        this.radius = radius;
+        this.radius = radius
     }
 
-    // define getter methods which will calculate each value using this.raidus and pi
-    get diameter() {
-        return 2 * this.radius; 
+    get diameter(){
+        return 2 * this.radius
     }
 
-    set diameter( diameter ) {
+    set diameter(diameter) {
         this.radius = diameter / 2
-    } 
+    }
 
     get circumference() {
-        return ( this.radius * 2 ) * pi
+        return (this.radius * 2) * pi
     }
 
-    set circumference( circumference ) {
-        this.radius = circumference / ( pi * 2 )
+    set circumference(circumference){
+        this.radius = (circumference / pi) / 2
     }
 
-    get area() {
-        return pi * ( this.radius * this.radius )
+    get area(){
+        return (this.radius * this.radius) * pi
     }
 
-    set area( area ) {
-        this.radius = Math.sqrt( area / pi )
-    }
 }
